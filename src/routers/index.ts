30c4dde2c14b2eb2d11226378cf75e4f19a52b4e
chris <cbcr93@gmail.com>
2022-07-24 16:34:55 -0300

    Heroku

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,10 +4,12 @@ import ordersRoutes from "./orders/orders.routes";
 import ticketsRoutes from "./tickts/tickets.routes";
 import userRoutes from "./users/users.routes";
 
-const routes = Router();
+
+const routes = require('express').Router();
 routes.use("/users", userRoutes);
 routes.use("/login", loginRoutes);
 routes.use("/tickets", ticketsRoutes);
 routes.use("/orders", ordersRoutes)
    
 export default routes;
+
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import AppDataSource from "./data-source";
 const init = async () => {
   const PORT = process.env.PORT || 3000;
   await AppDataSource.initialize();
-  app.listen(PORT, () => console.log(`They see me rollin' on port ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`They see me rollin' on port ${PORT}`);
+
+  });
 };
 
 init();
